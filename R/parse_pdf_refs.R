55412ccd1f990e92c56cd5bd05beef228555948b
abeltranandrade <abeltranandrade@smith.edu>
2023-02-19 11:55:35 -0500

    CLeaned up file from unecessary code

--- R/parse_pdf_refs.R
+++ R/parse_pdf_refs.R
@@ -13,7 +13,7 @@ parse_pdf_refs <- function(file) {
   # translate return from Raw to Characters
   output <- rawToChar(GROBID$content)
 
-  f_bib <- paste0("/Users/likamikhelashvili/Documents/Classes@Smith/SDS410/citationProfileR/extractedCitations/Test", ".bib")
+  f_bib <- paste0("/Users/adrianabeltran/Desktop/Academics/Spring23/Capstone/citationProfileR/extractedCitations", ".bib")
   writeLines(output, f_bib)
 
   #if no references are fine return that
@@ -28,21 +28,6 @@ parse_pdf_refs <- function(file) {
 
 #testers to run just the function on top
 #file <- "/Users/adrianabeltran/Downloads/brewerPaper.pdf"
-file <- "/Users/likamikhelashvili/Documents/Classes@Smith/thesis/Text/doleac_2017.pdf"
+file <- "/Users/adrianabeltran/Downloads/practicing-moderation-community-moderation-as-reflective-practice (1).pdf"
 output <- parse_pdf_refs(file)
 
-f_bib <- paste0("/Users/adrianabeltran/Downloads/CitationTest", ".bib"). #add .bib extension to file name (do not need file_path_blah since we are asking for it just for rn bc we do not have a shiny upload)
-bib <- parse_pdf_refs(f) #read the PDF
-writeLines(bib, f_bib) # write what you find in bib onto f_bib file
-
-
-f_bib <- paste0("~/citationProfileR/extractedCitations/", bib_name , ".bib")
-
-
-# translating this
-# def parse_pdf_refs(file):
-#   import requests
-# GROBID_URL = 'https://cloud.science-miner.com/grobid'
-# url = '%s/api/processReferences' % GROBID_URL
-# xml = requests.post(url, files={'input': open(file, 'rb')}, headers = {"Accept": "application/x-bibtex"}, data = {"consolidateCitations": "1"})
-# return xml.text
