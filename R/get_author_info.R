9d30aa369b051fae74c14b9dc7e45e76839695da
abeltranandrade <abeltranandrade@smith.edu>
2023-03-01 16:34:51 -0500

    Creating skeleton for the function to get the full names of the cited authors using crossRef API

---
+++ R/get_author_info.R
@@ -0,0 +1,12 @@
+#' get_author_info
+#'
+#' @param df: data frame containing author, title and date
+#'
+#' @return dataframe with the first and last name of the cited authors
+#' @export
+#'
+#' @examples
+get_author_info <-function(df){
+
+
+}
