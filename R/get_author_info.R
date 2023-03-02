ca534ddb0e5dd060600d2491f4d5e9ea6c119910
abeltranandrade <abeltranandrade@smith.edu>
2023-03-01 21:26:52 -0500

    Adding test CSV and our exploration work for crossref. Not working

--- R/get_author_info.R
+++ R/get_author_info.R
@@ -1,3 +1,5 @@
+library(rcrossref)
+
 #' get_author_info
 #'
 #' @param df: data frame containing author, title and date
@@ -7,6 +9,26 @@
 #'
 #' @examples
 get_author_info <-function(df){
+  df <- read_csv("R/test_citations_table.csv")
 
+  df <- df %>%
+    select(AUTHOR, TITLE, DATE, YEAR)
+
+
+  practice_author <- df$AUTHOR[6]
+  practice_title <- df$TITLE[6]
+  practice_date <- df$DATE[6]
+
+  practice_author <- strsplit(practice_author, split = ",")[[1]] #https://www.programmingr.com/tutorial/how-to-use-strsplit-in-r/ (list in list problem)
+  practice_author <- practice_author[1]
+
+ info <- cr_works(flq = c(query.author = practice_author, query.bibliographic = practice_title))
+
+ res_json <- cr_works_(flq = c(query.author = practice_author, query.bibliographic = practice_title))
+ unname(vapply(res_json, class, ""))
+ lol <- jsonlite::fromJSON(res_json[[1]])
+
+ hehe <- info[[data]]
 
 }
+
