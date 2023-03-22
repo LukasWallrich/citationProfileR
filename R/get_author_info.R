f9975e8a15b3a700b82c3fd3ae4a5fe26a0c039d
abeltranandrade <abeltranandrade@smith.edu>
2023-03-22 16:55:29 -0400

    Formatting into matrid and getting initial and last name

--- R/get_author_info.R
+++ R/get_author_info.R
@@ -16,13 +16,20 @@ get_author_info <-function(df){
 
   #practice examples
   practice_author <- df$AUTHOR[6]
+  print(practice_author)
   practice_title <- df$TITLE[6]
   practice_date <- df$DATE[6]
 
   # splitting the last name and initial to get only last name
-  practice_author <- strsplit(practice_author, split = ",")[[1]] #https://www.programmingr.com/tutorial/how-to-use-strsplit-in-r/ (list in list problem)
-  practice_author <- practice_author[1]
-
+  #practice_author <- strsplit(practice_author, split = ",")[[1]] #https://www.programmingr.com/tutorial/how-to-use-strsplit-in-r/ (list in list problem)
+  practice_author <- strsplit(practice_author, split = ", ")
+  practice_author <- matrix(unlist(practice_author),ncol=2,byrow=T)
+  print("first")
+  print(practice_author)
+  practice_last_name <- practice_author[1, 1]
+  print(practice_author_last_name)
+  practice_initial <- practice_author[1, 2]
+  print(practice_initial)
   #relevance/score is the score of the  match from the paper it returns
   #add the year of publication
  info <- rcrossref::cr_works(flq = c(query.author = practice_author, query.bibliographic = practice_title), limit = 3,sort='relevance', select = c('DOI', 'title', 'author', 'created', 'published-print', 'published-online', 'publisher-location'))
