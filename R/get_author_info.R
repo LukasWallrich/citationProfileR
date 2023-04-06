eaae38428338dfc2bb0d786dfc0a4c392997f262
lmikhelashvili <likamikhela@gmail.com>
2023-04-06 14:38:01 -0400

    working on the na doi list errors

--- R/get_author_info.R
+++ R/get_author_info.R
@@ -62,85 +62,92 @@ get_author_info <-function(df){
     filter(is.na(DOI))
 
 
-finished <- data.frame(matrix(nrow = 0, ncol = 6))
-column_names <- c("Author","Title", "Year", "Given", "Family", "Sequence")
-colnames(finished) = column_names
+  finished <- data.frame(matrix(nrow = 0, ncol = 6))
+  column_names <- c("Author","Title", "Year", "Given", "Family", "Sequence")
+  colnames(finished) = column_names
 
 
-for(entry in 1:nrow(na_list_doi)){
-  #pull the author, title, date entries from the NA-DOI data
-  author <- na_list_doi$AUTHOR[entry]
-  title <- na_list_doi$TITLE[entry]
-  date <- na_list_doi$YEAR[entry]
+  for(entry in 1:nrow(na_list_doi)){
+    #pull the author, title, date entries from the NA-DOI data
+    author <- na_list_doi$AUTHOR[entry]
+    title <- na_list_doi$TITLE[entry]
+    date <- na_list_doi$YEAR[entry]
 
-  print("we are on entry")
-  print(entry)
+    print("we are on entry")
+    print(entry)
 
-  print("author is ")
-  print(author)
+    print("author is ")
+    print(author)
 
-  #test cases for the different available author/title info for each paper
-  #both title and author have values
-  test_na_doi <-   if (!is.na(author) & !is.na(title)){
-    rcrossref::cr_works(flq = c(query.author = author, query.bibliographic = title), limit = 1,sort='relevance', select = c('DOI', 'title', 'author', 'created', 'published-print', 'published-online', 'publisher-location'))
-    #when the entry has a title and NA author
-  }
-  else if(!is.na(title) & is.na(author)){
-    rcrossref::cr_works(flq = c(query.bibliographic = title), limit = 1,sort='relevance', select = c('DOI', 'title', 'author', 'created', 'published-print', 'published-online', 'publisher-location'))
-    #when the entry has NA-value title and non-NA value author
-  }
-  else if(is.na(title) & !is.na(author)){
-    NA
-  }#we only get the most relevant match based on rcrossref's `relevance` sorting. We do not check that the titles match each other.
+    #test cases for the different available author/title info for each paper
+    #both title and author have values
+    date_title <- if(!is.na(date) & !is.na(title)){
+      paste0(title, " ", date)
+    } else{
+        title
+      }
 
-  print("test na doi is ")
-  print(test_na_doi)
+    test_na_doi <-   if (!is.na(author) & !is.na(title)){
+      rcrossref::cr_works(flq = c(query.author = author, query.bibliographic = date_title), limit = 1,sort='relevance', select = c('DOI', 'title', 'author', 'created', 'published-print', 'published-online', 'publisher-location'))
+      #when the entry has a title and NA author
+    }
+    else if(!is.na(title) & is.na(author)){
+      rcrossref::cr_works(flq = c(query.bibliographic = date_title), limit = 1,sort='relevance', select = c('DOI', 'title', 'author', 'created', 'published-print', 'published-online', 'publisher-location'))
+      #when the entry has NA-value title and non-NA value author
+    }
+    else if(is.na(title) & !is.na(author)){
+      NULL
+    }#we only get the most relevant match based on rcrossref's `relevance` sorting. We do not check that the titles match each other.
 
-
-  #if we pulled info from crossref
-  if(!is.null(test_na_doi)){
-
-    #get API payload
-    data_returned <- test_na_doi$data
-    print("data returned is")
-    print(data_returned)
-
-    print("The author data is")
-    print(data_returned[[5]][[1]])
-
-    print("The dims are")
-    print(dim(data_returned))
-
-    print("The nrow is")
-    print(nrow(data_returned))
+    print("test na doi is ")
+    print(test_na_doi)
 
 
+    #if we pulled info from crossref
+    if(!is.null(test_na_doi)){
 
-    # if the rcrossref returned no results, just add no results matched to that entry
-    if(is.null(data_returned) | nrow(data_returned)== 0){ #is.null(dim(data_returned)). # we have to check for both cases, if we get a null return or if we get a return but it is empty
-      print("I am getting into the null dim if statement")
-      first_name <- "No result matched"
+      #get API payload
+      data_returned <- test_na_doi$data
+      print("data returned is")
+      print(data_returned)
+
+      print("The author data is")
+      #print(data_returned[[5]][[1]])
+
+      print("The dims are")
+      print(dim(data_returned))
+
+      print("The nrow is")
+      print(nrow(data_returned))
+
+
+
+      # if the rcrossref returned no results, just add no results matched to that entry
+      if(is.null(data_returned) | nrow(data_returned)== 0){ #is.null(dim(data_returned)). # we have to check for both cases, if we get a null return or if we get a return but it is empty
+        print("I am getting into the null dim if statement")
+        first_name <- "No result matched"
+        finished[nrow(finished) + 1,] = c(author, title, date, first_name, NA, NA)
+        #move on to new entry
+        next
+      }
+
+
+      crossref_info <- data_returned[[5]][[1]] #pull(data_returned, author) this gives error need to fix
+      #create a duplicate row for each author of the paper
+      for(contributor in 1:nrow(crossref_info)){
+        finished[nrow(finished) + 1,] = c(author, title, date, crossref_info[contributor,1],crossref_info[contributor,2], crossref_info[contributor,3])
+      }
+    }
+    else{
+      #no information was gathered return NAs
+      first_name <- "inconclusive"
       finished[nrow(finished) + 1,] = c(author, title, date, first_name, NA, NA)
-      #move on to new entry
-      next
     }
+    print("Last updated finished is")
+    print(finished)
 
-    crossref_info <- data_returned[[5]][[1]]
-    #create a duplicate row for each author of the paper
-    for(contributor in 1:nrow(crossref_info)){
-      finished[nrow(finished) + 1,] = c(author, title, date, crossref_info[contributor,1],crossref_info[contributor,2], crossref_info[contributor,3])
-    }
   }
-  else{
-    #no information was gathered return NAs
-    first_name <- "inconclusive"
-    finished[nrow(finished) + 1,] = c(author, title, date, first_name, NA, NA)
-  }
-  print("Last updated finished is")
-  print(finished)
-
-}
-#doi_no_doi_data <- rbind(finished, info_dois) #this is for when we get the list of names for the doi-present entries
-return(finished)
+  #doi_no_doi_data <- rbind(finished, info_dois) #this is for when we get the list of names for the doi-present entries
+  return(finished)
 }
 
