c082db706b17f3eea215d9d1449e857aaf5bce47
abeltranandrade <abeltranandrade@smith.edu>
2023-04-26 01:02:29 -0400

    Added .on_load function that will use dev tools to install the github only packages. Gives a warning but I think this might make us pass the CMD checks on the github sides. It is running with only CMD check warnings of the files in directory I had mention and the "bad practice" warning from the on_load.

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+# install these from github, not CRAN:
+# Code from Jthorpe with some modifications https://stackoverflow.com/questions/33764036/include-github-packages-as-imports-in-description
+# dyplr code https://github.com/tidyverse/dplyr/blob/bbcfe99e29fe737d456b0d7adc33d3c445a32d9d/R/zzz.r
+.onLoad <- function(libname, pkgname) {
+  pkglist <- list(
+    c(name='bbr',url='metrumresearchgroup/bbr'))
+
+  for(pkg in pkglist)
+    if(!suppressWarnings(suppressPackageStartupMessages(require(pkg['name'],
+                                                                quietly=TRUE,character.only=TRUE)))){
+      devtools::install_github(pkg['url'])
+      suppressPackageStartupMessages( library(pkg['name'],character.only=TRUE))
+    }
+}
