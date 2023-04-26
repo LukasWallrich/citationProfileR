3368ff190e4ed0d24a254a84680a0e9ca5a0b920
abeltranandrade <abeltranandrade@smith.edu>
2023-04-26 01:28:46 -0400

    Readding remote and additional repository to the DESCRIPTION and adding run_on_load that I saw here https://rlang.r-lib.org/reference/on_load.html

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@
 # Code from Jthorpe with some modifications https://stackoverflow.com/questions/33764036/include-github-packages-as-imports-in-description
 # dyplr code https://github.com/tidyverse/dplyr/blob/bbcfe99e29fe737d456b0d7adc33d3c445a32d9d/R/zzz.r
 .onLoad <- function(libname, pkgname) {
+  run_on_load()
   pkglist <- list(
     c(name='bbr',url='metrumresearchgroup/bbr'))
 
