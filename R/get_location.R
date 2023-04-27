afd4eea3812b3036093e4e1e07fb3df182cbe46a
lmikhelashvili <likamikhela@gmail.com>
2023-04-27 10:47:18 -0400

    remove the data loading, add country_name col, remove return() from inside the For loop

--- R/get_location.R
+++ R/get_location.R
@@ -10,11 +10,15 @@
 
 get_location <- function(df_with_affiliation){
 
-  #load in the sample result from the crossref_api()
-  crossref_data <- read_csv("R/crossref_results.csv") %>%
-    select(-`...1`)
+  #RUN THIS IF YOU WANT TO LOAD A SAMPLE DF
+  # crossref_data <- read_csv("R/crossref_results.csv")
+
+
   #add an empty column country_code where the for loop will add values
   crossref_data <- cbind(crossref_data, country_code=NA)
+
+  #add an empty column country_name where the loop will convert country codes to country names
+  crossref_data <- cbind(crossref_data, country_name=NA)
 
   for(entry in 1:nrow(crossref_data)){
     #pull the specific affiliation one by one
@@ -25,12 +29,12 @@ get_location <- function(df_with_affiliation){
     #if the affiliation entry is not NA
     if(!is.na(affiliation)){
       #use the `countrycode` package to see if the country name is returned
+      #countrycode_pkg_return is a country name
       countrycode_pkg_return <- countrycode::codelist$country.name.en[stringr::str_detect(affiliation, stringr::regex(countrycode::codelist$country.name.en.regex, ignore_case = TRUE))]
-
       print(c("Got location code", countrycode_pkg_return))
 
         #if `countrycode` package does not return any information, use OSM
-        if(is_empty(countrycode_pkg_return)){
+        if(rlang::is_empty(countrycode_pkg_return)){
           #get the information about the location using OSM
           OSM_affiliation_returned <- geocode_OSM(affiliation)
 
@@ -66,16 +70,29 @@ get_location <- function(df_with_affiliation){
                 #get the location code
                 last_phrase_country_code <- last_phrase_location[[1]]$country_code
                 crossref_data$country_code[entry] <-  toupper(last_phrase_country_code)
+
+                #HERE get the country name from last_phrase_location
+                last_phrase_country_name <- last_phrase_location[[1]]$country
+                crossref_data$country_name[entry] <- last_phrase_country_name
+
+
                 next
               }else{#if after comma splitting OSM does not return anything
                 #put NA in the entry
                 crossref_data$country_code[entry] <-  NA
+                #HERE Put NA in country_name[entry]
+                crossref_data$country_name[entry] <-  NA
+
                 next
               }
 
             }else{# if the affiliation has no commas and OSM does not return anything
               #put NA into this entry and move to the next one
               crossref_data$country_code[entry] <-  NA
+
+              #HERE put NA in country_name[entry]
+              crossref_data$country_name[entry] <-  NA
+
               next
             }
 
@@ -90,9 +107,16 @@ get_location <- function(df_with_affiliation){
             #add this country code to the respective column
             crossref_data$country_code[entry] <-  toupper(list_country_code)
             print(crossref_data$country_code[entry] )
+            #HERE put value into country_name from location_list
+            list_country_name <- location_list[[1]]$country
+            crossref_data$country_name[entry] <- list_country_name
+
             next
           }
         }else{
+          #HERE put country name from countrycode_pkg_return into the column
+          crossref_data$country_name[entry] <-  countrycode_pkg_return
+
           #countrycode returns country names, turn it into 2 letter country codes
           countrycode_pkg_return <- toupper(countrycode(countrycode_pkg_return, origin = 'country.name', destination = 'iso2c'))
           #put the info returned by the `countrycode` pkg in the df
@@ -102,10 +126,14 @@ get_location <- function(df_with_affiliation){
 
     }else{     #if the entry is NA
       crossref_data$country_code[entry] <-  NA
+      #HERE put NA in country_name[entry]
+      crossref_data$country_name[entry] <-  NA
+
       next
     }
-  return(crossref_data)
   }#for loop
+  #TO DO: Once all the country codes are ready, add a new column that converts code to country name
 
 
+return(crossref_data)
 }#function
